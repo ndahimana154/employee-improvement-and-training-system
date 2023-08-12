9245deffd05d63a314075bcb135efc7b27060b7d
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-08-12 18:12:07 +0300

    Updated the footer

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <p>&copy; 2023 Your Company. All rights reserved.</p>
+                <p>&copy; 2023 . All rights reserved.</p>
             </div>
             <div class="col-md-6 text-md-right">
                 <p>Contact Us | Privacy Policy</p>
