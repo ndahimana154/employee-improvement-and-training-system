f8b44a7ccc07e667f480e2cae3daac18dcfab124
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-08-30 00:05:52 +0300

    Updated the hero pagee

--- home.php
+++ home.php
@@ -16,44 +16,25 @@
     ?>
     
     <!-- Hero Section -->
-    <section id="hero">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="images/pexels-fox-1595385.jpg" class="d-block w-100" alt="Image 1">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h3>Welcome to the Employee Training System</h3>
-                        <p>Empower your team with knowledge and skills.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="images/pexels-pixabay-301920.jpg" class="d-block w-100" alt="Image 2">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h3>Enhance Your Skills</h3>
-                        <p>Explore our diverse range of training modules.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="images/pexels-pixabay-355948.jpg" class="d-block w-100" alt="Image 3">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h3>Effective Communication</h3>
-                        <p>Connect with colleagues and trainers seamlessly.</p>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+    <section id="hero" class="row bg-white">
+        <div class="col-md-2"></div>
+        <div id="welcome" class="col-md-8 p-3" style="margin-top: 80px;
+            ">
+            <h2 class="text-primary h4 p-3" style="">
+            <span class="te">
+                <!-- WELCOME TO -->
+            </span> <br>
+            Employee Trainings and Imporvement System
+            </h2>
+            <p class="p-3">
+            Discover the Employee Training System, a dynamic platform offering a diverse array 
+            of expert-curated training courses. Unleash your potential with flexible learning, 
+            interactive assessments, and progress tracking. From programming languages to management 
+            strategies, our user-friendly interface empowers both beginners and seasoned professionals 
+            to learn at their own pace. Engage in collaborative discussions, earn certifications, and 
+            stay aligned with industry trends. Elevate your skills, advance your career, and thrive in 
+            an ever-evolving landscape of knowledge and expertise.
+            </p>
         </div>
     </section>
     
