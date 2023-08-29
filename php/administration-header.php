55be84e530f62f52275714d05c3b4f47a51f8daf
Ndahimana Bonheur <bonheurndahimana125@gmail.com>
2023-08-29 19:58:17 +0300

    CHanged logo on the administration

--- php/administration-header.php
+++ php/administration-header.php
@@ -2,7 +2,9 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="adminstration-home.php">
-                    <img src="logo.png" alt="Logo" width="50" height="50">
+                    <h1 class="h1 text-primary">
+                        EITS
+                    </h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
